2505788b513672a4ac0522b6ce55467f76649428
ManishChaudhary-0 <65691497+ManishChaudhary-0@users.noreply.github.com>
2020-08-29 22:38:17 -0400

    Update OffenceBehavior.java

--- src/strategydesignpattern/OffenceBehavior.java
+++ src/strategydesignpattern/OffenceBehavior.java
@@ -1,5 +1,7 @@
 package strategydesignpattern;
-
+    /*
+   Interface for Offence Behavior calling the play method in all of classes it is implemented in.
+    */
 public interface OffenceBehavior {
     public String play();
 }
